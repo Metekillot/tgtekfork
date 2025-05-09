66f58828d2f5ef515939bbe5eb138d38825c36d5
Joshua Kidder <anticitizenthree@gmail.com>
2025-05-09 19:42:20 -0400

    WIP on the card

--- tgui/packages/tgui/interfaces/ChameleonCardForge.tsx
+++ tgui/packages/tgui/interfaces/ChameleonCardForge.tsx
@@ -1,4 +1,5 @@
-import { Button, Dropdown, Input, Section, Stack } from 'tgui-core/components';
+import { useState } from 'react';
+import { Button, Section, Stack } from 'tgui-core/components';
 import { BooleanLike } from 'tgui-core/react';
 
 import { useBackend } from '../backend';
@@ -8,8 +9,67 @@ type Data = {
   current_name: string;
   current_assignment: string;
   current_trim: string;
-  current_age: string;
+  current_age: number;
   currently_wallet_spoofing: BooleanLike;
+};
+
+type TextInputSectionProps = {
+  holding_section: typeof Section;
+};
+
+
+
+const NameSection = (
+  initial_value: string) =>
+    {
+
+    }
+
+const AssignmentSection = (
+  initial_value: string) => {
+
+    const TrimSection = (
+      initial_value: number) => {
+
+const SpoofingSection = (
+  initial_value: BooleanLike) => {
+
+const AgeSection = (
+  initial_value: number) => {
+
+
+const ForgedSection = (
+  section_to_forge: string,
+  initial_value: string | number | BooleanLike,
+  props
+) => {
+  const { act, data } = useBackend<Data>();
+
+  const [fieldModified, setFieldModified] = useState<string>(section_to_forge);
+  const [fieldValue, setFieldValue] = useState<string | number | BooleanLike>(
+    initial_value,
+  );
+
+  const [randomizableToggle, setRandomizableToggle] =
+    useState<BooleanLike>(false);
+
+  const Randomize = (to_randomize: string) => {
+    return (
+      act('randomize', { to_randomize: fieldModified })
+    );
+  };
+
+  return (
+    <Stack.Item>
+      <Section
+        title={section_to_forge.charAt(0).toUpperCase() + section_to_forge.slice(1)}
+      >
+        <Button onClick={() => Randomize(section_to_forge)}>
+          Randomize
+        </Button>
+      </Section>
+    </Stack.Item>
+  );
 };
 
 export const ChameleonCardForge = (props) => {
@@ -24,44 +84,30 @@ export const ChameleonCardForge = (props) => {
 
   return (
     <Window width={500} height={300} theme="syndicate">
-      <Window.Content scrollable>
-        <Stack width="100%" height="50%">
-          <Section title="Name">
-            <Stack.Item inline width="33%" height="100%">
-              <Input value={current_name} placeholder="Enter name" />
-            </Stack.Item>
-          </Section>
-          <Section title="Assignment">
-            <Stack.Item inline width="33%" height="100%">
-              <Input value={current_assignment} placeholder="Enter job" />
-            </Stack.Item>
-          </Section>
-          <Section title="ID Trim">
-            <Stack.Item inline width="33%" height="100%">
-              <Dropdown
-                options={['One', 'Two', 'Three']}
-                overflow="visible"
-                over
-                menuWidth={20}
-                selected={null}
-                onSelected={() => {}}
-              />
-            </Stack.Item>
-          </Section>
-        </Stack>
-        <Stack width="100%" height="50%">
-          <Stack.Item width="50%" height="100%">
-            <Section title="ID Age">Age </Section>
+      <Window.Content fitted>
+        <Stack vertical inlineFlex justify="space-between">
+          <Stack.Item>
+            <Stack inlineFlex justify="space-between">
+              <Stack.Item>
+                <Section />
+              </Stack.Item>
+              <Stack.Item>
+                <Section />
+              </Stack.Item>
+              <Stack.Item>
+                <Section />
+              </Stack.Item>
+            </Stack>
           </Stack.Item>
-          <Stack.Item inline width="50%" height="100%">
-            <Section title="Wallet Spoofing">
-              <Button
-                content={
-                  currently_wallet_spoofing ? 'Stop Spoofing' : 'Start Spoofing'
-                }
-                color={currently_wallet_spoofing ? 'red' : 'green'}
-              />
-            </Section>
+          <Stack.Item>
+            <Stack inlineFlex justify="space-between">
+              <Stack.Item>
+                <Section />
+              </Stack.Item>
+              <Stack.Item>
+                <Section />
+              </Stack.Item>
+            </Stack>
           </Stack.Item>
         </Stack>
       </Window.Content>
