4c43cda03c5467faa5b5faab418f9a1f22d1c7dd
Joshua Kidder <anticitizenthree@gmail.com>
2025-05-13 23:00:57 -0400

    plugging away at the new UI

--- tgui/packages/tgui/interfaces/ChameleonCardForge.tsx
+++ tgui/packages/tgui/interfaces/ChameleonCardForge.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { Button, Section, Stack } from 'tgui-core/components';
+import { Input, Section, Stack } from 'tgui-core/components';
 import { BooleanLike } from 'tgui-core/react';
 
 import { useBackend } from '../backend';
@@ -13,62 +12,11 @@ type Data = {
   currently_wallet_spoofing: BooleanLike;
 };
 
-type TextInputSectionProps = {
-  holding_section: typeof Section;
-};
-
-
-
-const NameSection = (
-  initial_value: string) =>
-    {
-
-    }
-
-const AssignmentSection = (
-  initial_value: string) => {
-
-    const TrimSection = (
-      initial_value: number) => {
-
-const SpoofingSection = (
-  initial_value: BooleanLike) => {
-
-const AgeSection = (
-  initial_value: number) => {
-
-
-const ForgedSection = (
-  section_to_forge: string,
-  initial_value: string | number | BooleanLike,
-  props
-) => {
-  const { act, data } = useBackend<Data>();
-
-  const [fieldModified, setFieldModified] = useState<string>(section_to_forge);
-  const [fieldValue, setFieldValue] = useState<string | number | BooleanLike>(
-    initial_value,
-  );
-
-  const [randomizableToggle, setRandomizableToggle] =
-    useState<BooleanLike>(false);
-
-  const Randomize = (to_randomize: string) => {
-    return (
-      act('randomize', { to_randomize: fieldModified })
-    );
-  };
-
+const NameSection = (current_name: string) => {
   return (
-    <Stack.Item>
-      <Section
-        title={section_to_forge.charAt(0).toUpperCase() + section_to_forge.slice(1)}
-      >
-        <Button onClick={() => Randomize(section_to_forge)}>
-          Randomize
-        </Button>
-      </Section>
-    </Stack.Item>
+    <Section title="Name">
+      <Input>{current_name}</Input>
+    </Section>
   );
 };
 
@@ -85,31 +33,8 @@ export const ChameleonCardForge = (props) => {
   return (
     <Window width={500} height={300} theme="syndicate">
       <Window.Content fitted>
-        <Stack vertical inlineFlex justify="space-between">
-          <Stack.Item>
-            <Stack inlineFlex justify="space-between">
-              <Stack.Item>
-                <Section />
-              </Stack.Item>
-              <Stack.Item>
-                <Section />
-              </Stack.Item>
-              <Stack.Item>
-                <Section />
-              </Stack.Item>
-            </Stack>
-          </Stack.Item>
-          <Stack.Item>
-            <Stack inlineFlex justify="space-between">
-              <Stack.Item>
-                <Section />
-              </Stack.Item>
-              <Stack.Item>
-                <Section />
-              </Stack.Item>
-            </Stack>
-          </Stack.Item>
-        </Stack>
+        <Stack inlineFlex justify="space-between" />
+        <Stack inlineFlex justify="space-between" />
       </Window.Content>
     </Window>
   );
